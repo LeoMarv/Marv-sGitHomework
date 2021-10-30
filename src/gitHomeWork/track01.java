cb877d4700868c71fcf6fdbe664496d9ee60d78e
LeoMarvin <marvinleoneloliva@gmail.com>
2021-10-30 11:31:43 -0400

    New version of track 01 and 02

--- src/gitHomeWork/track01.java
+++ src/gitHomeWork/track01.java
@@ -1,10 +1,19 @@
 package gitHomeWork;
 
+import java.util.Scanner;
+
 public class track01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Hello World");
+		
+		System.out.println("What is your name?");
+		
+		Scanner scan=new Scanner(System.in);
+		
+		
 	}
 
 }
--- src/gitHomeWork/track02.java
+++ src/gitHomeWork/track02.java
@@ -5,6 +5,12 @@ public class track02 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	System.out.println("Hello World");
+	
+	
+	
+	
+	
 	}
 
 }
